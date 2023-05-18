8881187cff29a67ca03d8c4e1ddfbeb6c5dd3936
SamiUlHaq9 <96337429+SamiUlHaq9@users.noreply.github.com>
2023-05-18 15:24:02 +0500

    Remove DatatabletoJsonMethod method from all controller

--- Ecommerce-Project/Controllers/DiscountController.cs
+++ Ecommerce-Project/Controllers/DiscountController.cs
@@ -1,4 +1,5 @@
-﻿using Ecommerce_Project.Models;
+﻿using Ecommerce_Project.Common;
+using Ecommerce_Project.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -14,41 +15,6 @@ namespace Ecommerce_Project.Controllers
     [ApiController]
     public class DiscountController : ControllerBase
     {
-        [NonAction]
-        public static string DataTableToJSON(DataTable table)
-        {
-            var JSONString = new StringBuilder();
-            if (table.Rows.Count > 0)
-            {
-                JSONString.Append("[");
-                for (int i = 0; i < table.Rows.Count; i++)
-                {
-                    JSONString.Append("{");
-                    for (int j = 0; j < table.Columns.Count; j++)
-                    {
-                        if (j < table.Columns.Count - 1)
-                        {
-                            JSONString.Append("\"" + table.Columns[j].ColumnName.ToString() + "\":" + "\"" + table.Rows[i][j].ToString() + "\",");
-                        }
-                        else if (j == table.Columns.Count - 1)
-                        {
-                            JSONString.Append("\"" + table.Columns[j].ColumnName.ToString() + "\":" + "\"" + table.Rows[i][j].ToString() + "\"");
-                        }
-                    }
-                    if (i == table.Rows.Count - 1)
-                    {
-                        JSONString.Append("}");
-                    }
-                    else
-                    {
-                        JSONString.Append("},");
-                    }
-                }
-                JSONString.Append("]");
-            }
-            return JSONString.ToString();
-        }
-
         [HttpPost]
         [Route("InsertDiscount")]
         public async Task<object> InsertDiscount(Discount data)
@@ -69,7 +35,7 @@ namespace Ecommerce_Project.Controllers
         public string ReadAll()
         {
             Discount ra = new Discount();
-            var dtoj = DataTableToJSON(ra.ReadAll());
+            var dtoj = ExtensionHelper.DataTableToJSON(ra.ReadAll());
             return dtoj;
         }
 
@@ -80,7 +46,7 @@ namespace Ecommerce_Project.Controllers
         {
             Discount rbi = new Discount();
             rbi.Id = Id;
-            var b = DataTableToJSON(rbi.ReadById());
+            var b = ExtensionHelper.DataTableToJSON(rbi.ReadById());
             return b;
         }
 
--- Ecommerce-Project/Controllers/LoginController.cs
+++ Ecommerce-Project/Controllers/LoginController.cs
@@ -25,39 +25,6 @@ namespace Ecommerce_Project.Controllers
         {
             _config = config;
         }
-        public static string DataTableToJSON(DataTable table)
-        {
-            var JSONString = new StringBuilder();
-            if (table.Rows.Count > 0)
-            {
-                JSONString.Append("[");
-                for (int i = 0; i < table.Rows.Count; i++)
-                {
-                    JSONString.Append("{");
-                    for (int j = 0; j < table.Columns.Count; j++)
-                    {
-                        if (j < table.Columns.Count - 1)
-                        {
-                            JSONString.Append("\"" + table.Columns[j].ColumnName.ToString() + "\":" + "\"" + table.Rows[i][j].ToString() + "\",");
-                        }
-                        else if (j == table.Columns.Count - 1)
-                        {
-                            JSONString.Append("\"" + table.Columns[j].ColumnName.ToString() + "\":" + "\"" + table.Rows[i][j].ToString() + "\"");
-                        }
-                    }
-                    if (i == table.Rows.Count - 1)
-                    {
-                        JSONString.Append("}");
-                    }
-                    else
-                    {
-                        JSONString.Append("},");
-                    }
-                }
-                JSONString.Append("]");
-            }
-            return JSONString.ToString();
-        }
         [AllowAnonymous]
         [HttpPost]
         [Route("Login")]     
@@ -82,8 +49,6 @@ namespace Ecommerce_Project.Controllers
             {
                 return NotFound("User not found");
             }
-
-            
         }
         private string Generate(UserModel user)
         {
